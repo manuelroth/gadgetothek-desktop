b38423972e78a75e029d34d67d8caa68b4f194dc
Lukas Martinelli <me@lukasmartinelli.ch>
2014-10-06 20:38:48 +0200

    Added GadgetFrame and installed better look and feel

--- src/pl/App.java
+++ src/pl/App.java
@@ -1,13 +1,18 @@
 package pl;
 
-
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
-public class App 
-{
-    public static void main( String[] args ) throws IOException, InterruptedException, ExecutionException
-    {
-        System.out.println("Hello world");
+import javax.swing.SwingUtilities;
+
+import com.alee.laf.WebLookAndFeel;
+
+public class App {
+    public static void main( String[] args ) throws IOException, InterruptedException, ExecutionException {
+    	SwingUtilities.invokeLater(() -> {
+    		WebLookAndFeel.install();
+			MasterGadgetFrame frame = new MasterGadgetFrame();
+			frame.setVisible(true);
+        });
     }
 }
