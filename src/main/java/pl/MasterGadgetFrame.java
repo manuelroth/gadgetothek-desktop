7e1b43507b593f7cf5b76dfdd44b2e76e81a2aa2
Manuel Roth <manuelroth@hotmail.ch>
2014-10-13 15:12:54 +0200

    Update MasterGadgetFrame

--- src/main/java/pl/MasterGadgetFrame.java
+++ src/main/java/pl/MasterGadgetFrame.java
@@ -1,30 +1,84 @@
 package pl;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import java.awt.GridBagLayout;
+
+import javax.swing.JList;
+
+import java.awt.GridBagConstraints;
+
 import javax.swing.JButton;
 
-import java.awt.FlowLayout;
+import java.awt.Insets;
 
-@SuppressWarnings("serial")
-public class MasterGadgetFrame extends JFrame {
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+import bl.Gadget;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class GadgetMasterFrame extends JFrame {
 
 	private JPanel contentPane;
+	//private JFrame gadgetDetailFrame;
 
 	/**
 	 * Create the frame.
 	 */
-	public MasterGadgetFrame() {
+	public GadgetMasterFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JButton btnTest = new JButton("Test");
-		contentPane.add(btnTest);
+		JButton btnNewButton = new JButton("Gadget editieren");x
+		
+		JButton btnNewButton_1 = new JButton("Gadget erfassen");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//GadgetDetailFrame window = new GadgetDetailFrame(new Gadget(""));
+			}
+		});
+		
+		JPanel panel = new JPanel();
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(btnNewButton_1)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(btnNewButton))
+						.addComponent(panel, GroupLayout.DEFAULT_SIZE, 428, Short.MAX_VALUE))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnNewButton)
+						.addComponent(btnNewButton_1))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel, GroupLayout.DEFAULT_SIZE, 221, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JList list = new JList();
+		panel.add(list, BorderLayout.CENTER);
+		contentPane.setLayout(gl_contentPane);
 	}
-
 }
