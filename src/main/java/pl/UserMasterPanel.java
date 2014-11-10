6d5eefd3cb34da1442a39289817ac47b7b1225e2
khoepli <konrad.hoepli@gmail.com>
2014-11-10 14:58:52 +0100

    UserMasterPanel contains what used to be in BorrowMasterPanel to clone how it was dealt with on the UserDetailPane

---
+++ src/main/java/pl/UserMasterPanel.java
@@ -0,0 +1,90 @@
+package pl;
+
+import java.awt.Component;
+import java.awt.GridLayout;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.RowFilter;
+import javax.swing.RowFilter.ComparisonType;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.TableRowSorter;
+
+import bl.Library;
+
+public class UserMasterPanel extends JPanel {
+	private CustomerTableModel customerTableModel;
+    private TableRowSorter<CustomerTableModel> sorter;
+    private JTextField searchTextField;
+    private JTable customerTable = new JTable();
+
+	public UserMasterPanel(Library library) {
+		customerTableModel = new CustomerTableModel(library);
+		setBounds(100, 100, 450, 300);
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		initTable();
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		JPanel searchPanel = new JPanel();
+		searchPanel.setLayout(new GridLayout(1, 1, 0, 0));
+			
+		
+		searchTextField = new JTextField();
+ 		searchTextField.setToolTipText("Suchen...");
+ 		searchTextField.setText("Suchen...");
+ 		searchTextField.setColumns(10);
+		
+ 		searchTextField.addFocusListener(new FocusListener()
+ 		{
+ 			@Override
+ 			public void focusGained(FocusEvent e) {
+ 				searchTextField.setText("");
+ 			}
+ 			@Override
+ 			public void focusLost(FocusEvent e) {				
+ 			}			
+ 		});
+ 		searchTextField.addKeyListener(new KeyListener() {
+ 			@Override
+ 			public void keyPressed(KeyEvent arg0) {
+ 			}
+ 		
+ 			@Override
+ 			public void keyReleased(KeyEvent arg0) {
+ 				if (searchTextField.getText().length() != 0)
+ 				{
+ 					sorter = (TableRowSorter<CustomerTableModel>) customerTable.getRowSorter();
+ 					sorter.setRowFilter(RowFilter.regexFilter("(?i)" + searchTextField.getText(), 0, 1)); 
+ 			    }
+ 				else
+ 					sorter.setRowFilter(null);
+ 			}
+ 			@Override
+ 			public void keyTyped(KeyEvent arg0) {
+ 			}
+ 		});
+ 		searchPanel.add(searchTextField);
+ 		add(searchPanel);
+ 		
+ 		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setViewportView(customerTable);
+		add(scrollPane);		
+ 				
+	}
+	
+	private void initTable(){
+		customerTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		customerTable.setModel(customerTableModel);
+		
+		sorter = new TableRowSorter<CustomerTableModel>(customerTableModel);
+		customerTable.setRowSorter(sorter);
+	}
+}
